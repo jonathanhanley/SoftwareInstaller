907db08973a3c5b749ef258dd9fb193b9b017c2d
Jonathan <23277601+jonathanhanley@users.noreply.github.com>
2021-02-12 13:47:24 +0000

    Add files via upload

---
+++ install_software.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install snapd -y
+sudo snap install chromium
+sudo snap install pycharm-professional --classic
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install spotify
+sudo snap install noson
+sudo snap install brave
+
+sudo apt install latexila -y
+
